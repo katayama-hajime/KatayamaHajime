2b7cda76464f4536d2ffd40b807f770a513f43ca
HAJIM <HAJIM@curo0119>
2025-07-17 10:00:38 +0900

    九九の掛け算①

---
+++ HelloWorld_KataymaHajime/src/curriculum_B/Qes4.java
@@ -0,0 +1,14 @@
+package curriculum_B;
+
+public class Qes4 {
+	public static void main(String[] args) {
+		for (int i = 1; i <= 9; i++) {
+			for (int j = 1; j <= 9; j++) {
+				if (j == 9 ) {
+				System.out.print( String.format("%02d", i) +" * " + String.format("%02d", j) + " = " + String.format("%02d", i*j) );    
+				}else { System.out.print( String.format("%02d", i) +" * " + String.format("%02d", j) + " = " + String.format("%02d", i*j) +" ||  " );
+				}
+			}System.out.println();
+		}
+ }
+}
