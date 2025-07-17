e1400bb9506e485caf5341af4c103c405d36c2da
HAJIM <HAJIM@curo0119>
2025-07-17 10:01:02 +0900

    九九の掛け算②

---
+++ HelloWorld_KataymaHajime/src/curriculum_B/Qes5.java
@@ -0,0 +1,14 @@
+package curriculum_B;
+
+public class Qes5 {
+		public static void main(String[] args) {
+			for (int i = 1; i <= 9; i++) {
+				for (int j = 1; j <= 20; j++) {
+					if (j == 20 ) {
+					System.out.print( String.format("%03d", j) +" * " + String.format("%03d", i) + " = " + String.format("%03d", i*j) );    
+					}else { System.out.print( String.format("%03d", j) +" * " + String.format("%03d", i) + " = " + String.format("%03d", i*j) +" ||  " );
+					}
+				}System.out.println();
+            }
+		}
+}
