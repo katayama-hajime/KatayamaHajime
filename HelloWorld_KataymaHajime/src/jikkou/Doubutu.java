c5afeafb5fe082a1536387f6fa24199e627c0fd9
HAJIM <HAJIM@curo0119>
2025-08-23 22:46:14 +0900

    新規 8月23日課題1.23

---
+++ HelloWorld_KataymaHajime/src/jikkou/Doubutu.java
@@ -0,0 +1,11 @@
+package jikkou;
+import syori.Doubutuprocesu;
+public class Doubutu {
+	public static void main(String[] args) {
+		Doubutuprocesu raion = new Doubutuprocesu();
+		raion.setName("ライオン");
+		raion.setTaityou(2.1);
+		raion.setHayasa(80);
+		raion.syuturyoku();
+	}
+}
