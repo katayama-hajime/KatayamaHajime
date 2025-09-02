afbb67ae0d03c750e0d0e0f25cef33b864077138
HAJIM <HAJIM@curo0119>
2025-09-02 17:03:00 +0900

    こんにちは

--- HelloWorld_KataymaHajime/src/curriculum_B/Qes4.java
+++ HelloWorld_KataymaHajime/src/curriculum_B/Qes4.java
@@ -4,11 +4,15 @@ public class Qes4 {
 	public static void main(String[] args) {
 		for (int i = 1; i <= 9; i++) {
 			for (int j = 1; j <= 9; j++) {
-				if (j == 9 ) {
-				System.out.print( String.format("%02d", i) +" * " + String.format("%02d", j) + " = " + String.format("%02d", i*j) );    
-				}else { System.out.print( String.format("%02d", i) +" * " + String.format("%02d", j) + " = " + String.format("%02d", i*j) +" ||  " );
+				if (j == 9) {
+					System.out.print(String.format("%02d", i) + " * " + String.format("%02d", j) + " = "
+							+ String.format("%02d", i * j));
+				} else {
+					System.out.print(String.format("%02d", i) + " * " + String.format("%02d", j) + " = "
+							+ String.format("%02d", i * j) + " ||  ");
 				}
-			}System.out.println();
+			}
+			System.out.println();
 		}
- }
+	}
 }
